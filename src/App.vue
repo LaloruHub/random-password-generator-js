fa08c04c576ba0fc46dea2af13e9a9a18d16a2d9
PC_Laloru\Laloru <gonzaloazaldegiruiz@gmail.com>
2023-03-02 21:46:37 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<main class="">
+		<label for="char">Caracteres: </label>
+		<select
+			name="char"
+			id="char"
+      v-model="count"
+      @change="log">
+			<option
+				v-for="char in maxChar"
+				:value="char">
+				{{ char }}
+			</option>
+		</select>
+	</main>
+</template>
+
+<script>
+	export default {
+		mounted() {},
+		data() {
+			return {
+				maxChar: 25,
+				chars: '',
+        count: '',
+			};
+		},
+		methods: {
+      log(){
+        console.log(this.count);
+      }
+    },
+		computed: {},
+	};
+</script>
